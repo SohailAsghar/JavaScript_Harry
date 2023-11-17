beb5809972354b9881c3a9900bb9657cd78195e0
sohail-asghar <sohailasghar.qmcc@gmail.com>
2023-11-17 23:25:20 +0500

    Initial Code for Beginning

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 15;
+var b = 20;
+console.log(a+b)
+
+var a = prompt("What is your age?")
+console.log(a)
+
+if(a<b){
+    console.log("You are under age!")
+}else{
+    console.log("You are qualified for this session!")
+}
